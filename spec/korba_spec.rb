f07795e541f479c2492a4c0ec5ad36467e708d4b
kk0000-kk <kazuyoshi.kobayashi.1@gmail.com>
2024-11-30 09:20:37 +0900

    first commit

---
+++ spec/korba_spec.rb
@@ -0,0 +1,11 @@
+# frozen_string_literal: true
+
+RSpec.describe Korba do
+  it "has a version number" do
+    expect(Korba::VERSION).not_to be nil
+  end
+
+  it "does something useful" do
+    expect(false).to eq(true)
+  end
+end
